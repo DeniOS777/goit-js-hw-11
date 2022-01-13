05355f4a7e987c21434e631312dc93c30b90b236
DeniOS777 <87602096+DeniOS777@users.noreply.github.com>
2022-01-13 22:06:06 +0000

    Deploying to gh-pages from @ DeniOS777/goit-js-hw-11@d273943560b68f9781e70cf7ceef258ea131c377 🚀

--- src.4448cc4f.js
+++ src.4448cc4f.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./css/styles.css");const s=300;
 },{"./css/styles.css":"krre"}]},{},["Focm"], null)
-//# sourceMappingURL=/goit-js-hw-11/src.ec9f4241.js.map
+//# sourceMappingURL=/goit-js-hw-11/src.4448cc4f.js.map
