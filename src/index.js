28ca42cfb752dc1df141c45a8dd3d817c6616a98
Denis Moskalenko <87602096+DeniOS777@users.noreply.github.com>
2022-01-14 18:10:10 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
+
+const refs = {
+  form: document.querySelector('.search-form'),
+  gallery: document.querySelector('.gallery'),
+};
+
+refs.form.addEventListener('submit', onButtonSearchImagesClick);
+
+function onButtonSearchImagesClick(e) {
+  e.preventDefault();
+  const searchQuery = e.currentTarget.elements.searchQuery.value.trim();
+  if (!searchQuery) {
+    return;
+  }
+  onFetchImages(searchQuery)
+    .then(images => {
+      console.log(images);
+      console.log(images.total);
+    })
+    .catch(error => console.log(error));
+}
+
+function onFetchImages(searchQuery) {
+  return fetch(
+    `https://pixabay.com/api/?key=25243201-da43b78e8715fb1cc3094e420&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true`,
+  ).then(resolve => {
+    if (resolve.status !== 200) {
+      throw new Error(error.message);
+    }
+    console.log(resolve);
+    return resolve.json();
+  });
+}
