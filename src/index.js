7f72fc598d6810c2a30d11bbf0393e8a4294af9f
Denis Moskalenko <87602096+DeniOS777@users.noreply.github.com>
2022-01-15 21:27:27 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -65,30 +65,5 @@ function successPayload({ totalHits }) {
 }
 
 function emptySearchQuery() {
-  Notify.info('Enter please keyword or words for begin search');
+  Notify.info('Field of search is empty, enter please keyword or words for begin search');
 }
-
-// function renderImages(images) {
-//   const markup = images
-//     .map(item => {
-//       return `<div class="photo-card">
-//   <img src="${item.webformatURL}" alt="${item.tags}" loading="lazy" />
-//   <div class="info">
-//     <p class="info-item">
-//       <b>Likes</b>${item.likes}
-//     </p>
-//     <p class="info-item">
-//       <b>Views</b>${item.views}
-//     </p>
-//     <p class="info-item">
-//       <b>Comments</b>${item.comments}
-//     </p>
-//     <p class="info-item">
-//       <b>Downloads</b>${item.downloads}
-//     </p>
-//   </div>
-// </div>`;
-//     })
-//     .join('');
-//   refs.gallery.insertAdjacentHTML('beforeend', markup);
-// }
